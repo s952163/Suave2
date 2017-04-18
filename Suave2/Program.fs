90dabe15780db525c826530330ecc6d808b42995
s952163 <s995110@yahoo.co.jp>
2017-04-18 13:52:47 +0900

    Add project files.

---
+++ Suave2/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Suave
+open System
+open System.Threading
+
+
+[<EntryPoint>]
+let main argv = 
+    let cts = new CancellationTokenSource()
+    let conf = { defaultConfig with cancellationToken = cts.Token}
+    let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
+
+    Async.Start(server,cts.Token)
+    printfn "Make Requests now"
+    Console.ReadKey true |> ignore
+    
+    cts.Cancel()
+
+    0
